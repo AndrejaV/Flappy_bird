910c7d23426a74d545a10fbdc39c8bf9036bfd6a
AndrejaV <andra964@Hotmail.com>
2021-09-01 18:49:49 +0200

    v 1.0

---
+++ Flappy Bird/Assets/Scripts/GameAssets.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GameAssets : MonoBehaviour
+{
+    private static GameAssets instanca;
+    public Transform prefPipeHead, prefPipeBody, prefGround;
+    public AudioZvuk[] nizZvukova;
+
+
+    public static GameAssets GetInstancu() {
+        return instanca;
+    }
+
+    private void Awake()
+    {
+        instanca = this;
+    }
+
+
+    
+
+
+
+    [System.Serializable]
+    public class AudioZvuk
+    {
+        public Sound.Zvuk zvuk;
+        public AudioClip audioklip;
+    }
+
+}
+
